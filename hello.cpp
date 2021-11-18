f6de138747e35984dcd7c74bdbf577c16371c772
A1esandr <a1exswan001@gmail.com>
2021-11-19 00:04:11 +0500

    added ex

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello World!";
-    return 0;
+using namespace std;
+
+int main()
+{
+    cout << "Hello World" << endl;
+
+    int myNum;
+    myNum = 15;
+    cout << myNum << endl;
+
+    int myAge = 35;
+    cout << "I am " << myAge << " years old." << endl;
+
+    int x = 5;
+    int y = 6;
+    int sum = x + y;
+    cout << sum << endl;
+
+    int x1 = 5, y1 = 6, z = 50;
+    cout << x1 + y1 + z << endl;
 }
