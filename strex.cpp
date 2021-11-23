d61e7f1b8680da33de12f9141c8a737d82fa501e
A1esandr <a1exswan001@gmail.com>
2021-11-24 00:04:15 +0500

    added ex

--- strex.cpp
+++ strex.cpp
@@ -14,5 +14,13 @@ int main()
    string firstName = "John ";
    string lastName = "Doe";
    string fullName = firstName.append(lastName);
-   cout << fullName;
+   cout << fullName << endl;
+
+   string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+   cout << "The length of the txt string is: " << txt.length() << endl;
+
+   string myString = "Hello";
+   cout << myString[1] << endl;
+   myString[0] = 'J';
+   cout << myString << endl;
 }
